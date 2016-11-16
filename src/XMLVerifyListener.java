3cd82156c877579b0988762e5b7d5ac19a52a299
Srdjan Miletic <SrdjanMiletic2@gmail.com>
2016-11-16 10:33:40 +0000

    Implemented logger.

--- src/XMLVerifyListener.java
+++ src/XMLVerifyListener.java
@@ -2,6 +2,10 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JTextArea;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,7 +18,8 @@ public class XMLVerifyListener implements ActionListener{
 	
 	private JTextArea txt;
 	private String fileText;
-	
+	private final static Logger logger = Logger.getLogger(XMLVerifyListener.class.getName());
+
 	public XMLVerifyListener(JTextArea txt){
 		this.txt = txt;
 	}
@@ -29,14 +34,20 @@ public class XMLVerifyListener implements ActionListener{
 		docFact.setNamespaceAware(true);
 		docFact.setValidating(false);
 		try{
+			logger.setLevel(Level.INFO);
+			logger.addHandler(new ConsoleHandler());
+			
 			DocumentBuilder docBuild = docFact.newDocumentBuilder();
 			docBuild.setErrorHandler(new ParseErrorHandler());
+			
 			Document doc = docBuild.parse(FileStorage.f);
 			doc.getDocumentElement().normalize();	//Collapses text on multiple lines.
+			
 			Element root = doc.getDocumentElement();
 			fileText = root.getTextContent();
 			txt.setForeground(Color.BLACK);
 			txt.setText(fileText);
+
 			System.out.println("Everything is a-okay :)");
 		}
 		catch(Exception e){
@@ -44,6 +55,7 @@ public class XMLVerifyListener implements ActionListener{
 			txt.setText("Something went really wrong\n" +
 						e.getMessage() +
 						" \nPlease try importing a well-formatted XML file");
+			logger.log(Level.SEVERE, e.toString());
 		}
 	}
 }
